037df9455c03d55bc9463fec6c53b419bb3b916a
Ricardo Teófilo Esteve Hiyelekwa <ricardo.hiyelekwa@minfin.gov.ao>
2024-08-19 14:50:43 +0100

    tarefas nextjs

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from './styles.module.css'
+
+export default function Dashboard(){
+    return(
+        <div className={styles.container}>
+            <Head>
+                <title>Meu painel de tarefas</title>
+            </Head>
+
+
+            <h1>Página painel</h1>
+        </div>
+    )
+}
